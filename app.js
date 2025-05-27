d0373718b0451dd30f117f1044e5c8e346d9e82c
islamrifatislam <islamrifatislam500@gmail.com>
2025-05-27 06:09:27 +0600

    added a applied visa done

--- app.js
+++ app.js
@@ -114,6 +114,79 @@ app.delete('/visa/:id', async (req, res) => {
           data: deletdData
      })
 })
+/ applied visa--------------//
+app.post("/applied-visa", async (req, res) => {
+     const payload = {
+          ...req?.body,
+          applied_date: new Date()
+     }
+     const result = await appliedVisa.insertOne(payload)
+     manageResponse(res, {
+          success: true,
+          statusCode: 201,
+          message: "visa appied successful",
+          data: result
+     })
+})
+app.get("/applied-visa/:email", async (req, res) => {
+     const { email } = req?.params
+     const result = await appliedVisa.find({ applicant_email: email }).toArray()
+     manageResponse(res, {
+          success: true,
+          statusCode: 200,
+          message: "My applied visa fetched",
+          data: result
+     })
+})
+app.patch("/applied-visa/update/:id", async (req, res) => {
+     const { id } = req?.params
+     const result = await appliedVisa.findOne({ _id: new ObjectId(id) })
+     if (!result) {
+          manageResponse(res, {
+               statusCode: 404,
+               success: false,
+               message: "Applied data not found",
+               data: null
+          })
+          return
+     }
+     const updatedData = await appliedVisa.updateOne
+          ({ _id: new ObjectId(id) }, {
+               $set: {
+                    ...req?.body
+               }
+          })
+
+     manageResponse(res, {
+          statusCode: 200,
+          success: true,
+          message: "Applied visa update successful",
+          data: updatedData
+     })
+})
+
+app.delete('/applied-visa/delete/:id', async (req, res) => {
+     const { id } = req?.params
+     const result = await appliedVisa.findOne({ _id: new ObjectId(id) })
+     if (!result) {
+          manageResponse(res, {
+               statusCode: 404,
+               success: true,
+               message: "Visa info not found",
+               data: null,
+
+          })
+          return
+     }
+     const deletdData = await appliedVisa.deleteOne({ _id: result._id })
+     manageResponse(res, {
+          statusCode: 200,
+          success: true,
+          message: "Applied visa info deleted!!",
+          data: deletdData
+     })
+})
+
 
 
 export default app;
